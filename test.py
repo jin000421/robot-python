b09be315e4891adc0f3378de3608bf9e1b0d5011
jin000421 <sujinsujin2@naver.com>
2025-02-12 11:35:52 +0900

    369_(흐름,주석)

--- test.py
+++ test.py
@@ -1,2 +1,27 @@
-print("hi")
-'''mamamama'''
+# num(순서)
+
+# (함수)
+#  누구의 순서인가? 
+    # user  
+    # com
+
+# (함수)
+#  3 6 9 인가? 
+    # 369 0 -> ture
+    # 369 x -> false
+
+# while문
+    # user  
+        # input 받기 
+        # num() == 369
+            # o -> input == '짝' 
+            # x -> break
+        # num != 369 
+            # o ->  input == num
+            # x -> break
+    # com 
+        # num == 369
+            # 짝 
+        # num != 369
+            # num
+# break -> game over
